d5a8c5b45442becac47eaf873c0945414977b3e1
aditichauhan2 <aditi.chauhan@card91.io>
2025-03-19 10:49:08 +0530

    change

--- src/apply/pages/page.js
+++
@@ -1,9 +0,0 @@
-import MultiStepForm from "../../components/MultiStepForm";
-
-export default function ApplyPage() {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <MultiStepForm />
-    </div>
-  );
-}
